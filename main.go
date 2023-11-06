c251a365d740e9514ade0d3fee345e7289226483
KevinED11 <mendez_kael@hotmail.com>
2023-11-05 19:34:54 -0700

    .

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	personMap := map[string]interface{}{
+		"name":    "John",
+		"age":     30,
+		"married": true,
+		"house": map[string]interface{}{
+			"address": "21 2nd Street",
+			"city":    "New York",
+			"state":   "NY",
+			"zip":     10021,
+		},
+	}
+
+	jsonData, err := json.Marshal(personMap)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("json data %s\n", string(jsonData))
 }
